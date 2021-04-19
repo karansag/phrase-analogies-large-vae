095aab367499dd581afb6fb648ec19376fb7f90e
Karan Sagar <ksagar1030@gmail.com>
2021-04-18 20:13:41 -0400

    Add base code for running and testing experiments

--- src/experiment.py
+++ src/experiment.py
@@ -1,5 +1,8 @@
 import csv
 
+import funcy as f
+import analogy as a
+
 
 def read_csv(filename):
     """
@@ -21,13 +24,35 @@ def read_csv(filename):
     return inputs, outputs
 
 
-def run_experiment(inputs, n_samples=1):
+def run_experiment(inputs, n_samples=1, temperature=1):
     """
     Runs experiment given inputs.
 
     Takes `n_samples` samples from the VAE
+    Returns a list of size `n_samples` of results for each input
     """
-    pass
+    encoder_data = a.get_encoder()
+    decoder_data = a.get_decoder()
+
+    vae = a.get_vae(
+        encoder_data["model"],
+        decoder_data["model"],
+        encoder_data["tokenizer"],
+        decoder_data["tokenizer"],
+        beta=0,
+    )
+
+    evaluator = f.partial(
+        a.eval_analogy, vae, encoder_data["tokenizer"], decoder_data["tokenizer"]
+    )
+    preds = [
+        map(
+            lambda inp: evaluator(inp[0], inp[1], inp[2], temperature=temperature),
+            inputs,
+        )
+        for _ in range(n_samples)
+    ]
+    return list(zip(*preds))
 
 
 def compute_statistics(preds, outputs):
@@ -37,10 +62,27 @@ def compute_statistics(preds, outputs):
     pass
 
 
+def get_pred_str(prediction):
+    """Returns prediction string for one prediction from data returned by run_experiment"""
+    return prediction[0][0]
+
+
 def write_output(output_filename, summary_filename, inputs, preds, outputs):
     """
     Writes experiment results to files
     `output_filename` for the per-trial inputs, outputs, and predictions
     `summary_filename` for the summary statistics
     """
-    pass
+    # For now: assume only one sample
+    with open(output_filename, "w", newline="") as csvfile:
+        writer = csv.writer(
+            csvfile, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL
+        )
+        for inp, pred, outp in zip(inputs, preds, outputs):
+            writer.writerow(inp + [get_pred_str(pred)] + [outp])
+
+
+def run(input_filename, output_filename):
+    inputs, outputs = read_csv(input_filename)
+    preds = run_experiment(inputs)
+    write_output(output_filename, "", inputs, preds, outputs)
