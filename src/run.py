d6f29a3c097ee61133e9a50d0aec178885ad28a0
Joshua C Rivera <39106451+JoshuaCRivera@users.noreply.github.com>
2021-04-26 16:16:40 -0400

    Add more intuitive run functions

---
+++ src/run.py
@@ -0,0 +1,27 @@
+import analogy
+import experiment
+
+
+def run_csv(input_filename, output_filename):
+
+	return experiment.run(input_filename, output_filename)
+	
+encoder = None
+decoder = None
+vae = None
+
+def set_vae():
+	global encoder
+	global decoder
+	global vae
+	encoder = analogy.get_encoder()
+	decoder = analogy.get_decoder()
+	vae = analogy.get_vae(encoder['model'], decoder['model'], encoder['tokenizer'], decoder['tokenizer'])
+	
+def run_single(a,b,c, temperature = 0.01):
+	if encoder == None or decoder == None:
+		set_vae()
+		print()
+
+	return analogy.eval_analogy(vae, encoder['tokenizer'], decoder['tokenizer'], a, b, c, temperature = temperature, degree_to_target=1)
+	
